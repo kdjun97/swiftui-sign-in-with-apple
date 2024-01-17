1e97f77267a8d5bb0c6c87695f7676db54ce9766
kdjun97 <kdjun97@gmail.com>
2024-01-17 21:09:47 +0900

    initial commit

---
+++ SignInWithAppleTest/SignInWithAppleTest/SignInWithAppleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleTestApp.swift
+//  SignInWithAppleTest
+//
+//  Created by 김동준 on 1/17/24
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
